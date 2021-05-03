477a81c58029db4cb5e95303b9adede6666664ea
nagehancapa <capanagehan@gmail.com>
2021-05-03 14:55:50 +0200

    added link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ function App() {
       <p>I liked javascript and react</p>
       <p>My first react app</p>
       <Title title="Test" description="My desciption"/>
-      <Title title="Another test"/>      
+      <Title title="Another test"/>
+      <a
+          className="App-link"
+          href="https://hungry-dubinsky-d541b2.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to my first website
+        </a>
     </div>
   );
 }
