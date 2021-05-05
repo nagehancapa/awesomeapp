5876cafc4b974dd55a750f6808f7e1adc715eebd
nagehancapa <capanagehan@gmail.com>
2021-05-05 11:25:30 +0200

    added some components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import './App.css';
-import Title from "./components/Title"
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
+      <main className="container my-5">
+        <Title title="Some example title" />
+        <div className="row">
+          {all_pokemon.map((pokemon, index) => (
+            <div key={index} className="col-md-6 col-lg-4">
+              <Pokemon
+                name={pokemon.name}
+                weight={pokemon.weight}
+                awesome={pokemon.awesome}
+                terrifying={pokemon.terrifying}
+                abilities={pokemon.abilities}
+              />
+            </div>
+          ))}
+        </div>
+      </main>
       <h1>Hello World!</h1>
       <p>I liked javascript and react</p>
       <p>My first react app</p>
-      <Title title="Test" description="My desciption"/>
-      <Title title="Another test"/>
+      <Title title="Test" description="My desciption" />
+      <Title title="Another test" />
       <a
-          className="App-link"
-          href="https://hungry-dubinsky-d541b2.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to my first website
-        </a>
+        className="App-link"
+        href="https://hungry-dubinsky-d541b2.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Welcome to my first website
+      </a>
     </div>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,11 @@
-import React from "react"
-import "./Title.css"
+import React from "react";
+import "./Title.css";
 
 export default function Title(props) {
-	return (
-		<div className="Title">
-			<h2>{props.title}</h2>
-			<h1>{props.description}</h1>
-		</div>
-	)
+  return (
+    <div className="Title">
+      <h2>{props.title}</h2>
+      <h1 className="mb-4">{props.description}</h1>
+    </div>
+  );
 }
